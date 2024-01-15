5119ab69369711465cff19d6ef3235e92c92195d
dennissammy77 <dennissammy77@gmail.com>
2024-01-15 23:41:50 +0300

    callback

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./src/lib/db";
 import { AuthConfig } from '@/auth.config';
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +10,30 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  callbacks: {
+    async session({session, token}){
+      console.log({sessionToken: token});
+
+      if (token.sub && session.user){
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user){
+        session.user.role = token.role;
+      }
+      return session
+    },
+    async jwt({token}){
+      if( !token?.sub) return token;
+
+      const existing_user = await getUserById(token.sub);
+
+      if (!existing_user) return token;
+
+      token.role = existing_user.role;
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt'},
   ...AuthConfig
